f14bc20d118e90b5d771e3c39680f525a084c6ed
zhangchenglin <469946668@qq.com>
2020-03-20 01:51:11 +0800

    统一代码风格、整理代码

--- member/sms/SendSms.php
+++ member/sms/SendSms.php
@@ -24,34 +24,34 @@ use AlibabaCloud\Client\Exception\ServerException;
 // Usage：https://github.com/aliyun/openapi-sdk-php/blob/master/README.md
 
 AlibabaCloud::accessKeyClient($accessKeyId, $accessSecret)
-    ->regionId($RegionId)
-    ->asDefaultClient();
+  ->regionId($RegionId)
+  ->asDefaultClient();
 
 try {
-    $request_result = AlibabaCloud::rpc()
-        ->product($request_json['product'])
-        ->scheme($request_json['scheme'])
-        ->version($request_json['version'])
-        ->action($request_json['action'])
-        ->method($request_json['method'])
-        ->host($request_json['host'])
-        ->options([
-            'query' => [
-                'RegionId' => $RegionId,
-                'PhoneNumbers' => $PhoneNumbers,
-                'SignName' => $SignName,
-                'TemplateCode' => $TemplateCode,
-                'TemplateParam' => $TemplateParam,
-                'SmsUpExtendCode' => $SmsUpExtendCode,
-                'OutId' => $OutId,
-            ],
-        ])
-        ->request();
-    $sms_request_result = $request_result->toArray();
+  $request_result = AlibabaCloud::rpc()
+    ->product($request_json['product'])
+    ->scheme($request_json['scheme'])
+    ->version($request_json['version'])
+    ->action($request_json['action'])
+    ->method($request_json['method'])
+    ->host($request_json['host'])
+    ->options([
+      'query' => [
+        'RegionId' => $RegionId,
+        'PhoneNumbers' => $PhoneNumbers,
+        'SignName' => $SignName,
+        'TemplateCode' => $TemplateCode,
+        'TemplateParam' => $TemplateParam,
+        'SmsUpExtendCode' => $SmsUpExtendCode,
+        'OutId' => $OutId,
+      ],
+    ])
+    ->request();
+  $sms_request_result = $request_result->toArray();
 } catch (ClientException $e) {
-    $sms_request_result['error']['ClientException'] = $e->getErrorMessage() . PHP_EOL;
+  $sms_request_result['error']['ClientException'] = $e->getErrorMessage() . PHP_EOL;
 } catch (ServerException $e) {
-    $sms_request_result['error']['ServerException'] = $e->getErrorMessage() . PHP_EOL;
+  $sms_request_result['error']['ServerException'] = $e->getErrorMessage() . PHP_EOL;
 }
 
 return $sms_request_result;
